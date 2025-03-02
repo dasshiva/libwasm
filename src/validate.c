6145d69a08942463ee937b8d042326839520fb39
Shivashish Das <dasshivashish11@gmail.com>
2025-03-02 11:47:49 +0530

    basic validation and compaction support

--- src/validate.c
+++ src/validate.c
@@ -1,7 +1,34 @@
 #include <libwasm.h>
 #include <stdio.h>
+
+static int compactIntoFunction();
+static int compactIntoMemory();
+static int compactIntoTable();
+static int compactIntoImport();
+static int prepareValidatedModule();
+
 int validateModule(struct WasmModule *module) {
     int tyidx = findSectionByHash(module, WASM_HASH_Type);
     int fnidx = findSectionByHash(module, WASM_HASH_Function);
+    return 1;
+}
+
+static int compactIntoFunction() {
+    return 1;
+}
+
+static int compactIntoMemory() {
+    return 1;
+}
+
+static int compactIntoTable() {
+    return 1;
+}
+
+static int compactIntoImport() {
+    return 1;
+}
+
+static int prepareValidatedModule() {
     return 1;
 }
