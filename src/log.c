e2377a1344a5f8ba17bb045a0c107713b5f67111
Shivashish Das <dasshivashish11@gmail.com>
2025-03-03 12:58:38 +0530

    small addition v2

--- src/log.c
+++ src/log.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+static const char* levelToString = {
+	[DEBUG] = "DEBUG",
+	[INFO] = "INFO",
+	[WARNING] = "WARNING",
+	[ERROR] = "ERROR",
+	[INTERNAL_ERROR] = "INTERNAL ERROR",
+	[FATAL] = "FATAL"
+};
+
 // We log to stdout as some devices don't have stderr
 void logStandardOutput(const char* file, const char* line, const char* func, int level, const char* format, ...) {
 	va_list ap;
 	va_start(ap, format);
+	printf("[%s] %s:%s %s ", levelToString[level], file, line, func);
 	vprintf(format, ap);
+	printf("\n");
+	va_end(ap);
 }
 
 void logStandardOutput(const char* file, const char* line, const char* func, int level, FILE* dest, const char* format, ...) {
